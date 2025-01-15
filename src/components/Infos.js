c7c901aae1e8ecd3594c3bbcb26d992ef9ed6978
Wesley Goarant <Wesleydu29860@gmail.com>
2025-01-15 19:18:33 +0100

    modifications de la page contact

--- src/components/Infos.js
+++ src/components/Infos.js
@@ -1,8 +1,24 @@
+import '../style/Infos.scss'
+
 function Infos() {
     return(
         <div>
             <h2>Contactez moi ! </h2>
-
+            <div className="infos-content">
+                <div class="phone">
+                    <i class="fa-solid fa-phone"></i>
+                    <p>07-65-81-41-72</p>
+                </div>
+                <div class="mail">
+                    <i class="fa-solid fa-envelope"></i>
+                    <p>claudehenry29@proton.me</p>
+                </div>
+                <div class="localisation">
+                    <i class="fa-solid fa-house"></i>
+                    <p>Linguennec 29140 Saint Yvy</p>
+                </div>
+                
+            </div>
         </div>
     );
 };
