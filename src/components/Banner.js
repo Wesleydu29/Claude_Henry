5e9122bca9ae4fd833702d1c05eb60e6e0f19d46
Wesley Goarant <Wesleydu29860@gmail.com>
2025-01-31 15:16:19 +0100

    amélioration de quelques détails visuels ainsi que des performances du site

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,8 +2,8 @@ import '../style/Banner.scss'
 
 function Banner({ backgroundImage, title, content }) {
     return(
-        <div className="parallax-container" style={{ backgroundImage: `url(${backgroundImage})` }} >
-            <div className="parallax-content">
+        <div className="banner" style={{ backgroundImage: `url(${backgroundImage})` }} loading='lazy' >
+            <div className="banner-content">
                 <h2>{title}</h2>
                 <p>{content}</p>
             </div>
