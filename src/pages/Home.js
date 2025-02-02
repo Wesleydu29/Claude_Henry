093ccd09f9b6f5f4e66242e2d4217c19870c1c2f
Wesley Goarant <Wesleydu29860@gmail.com>
2025-02-02 14:35:08 +0100

    installation de react helmet, ajout des ablises meta pour le SEO

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,22 @@ import Gallery from "../components/Gallery";
 import plage from "../assets/photo_plage.jpg"
 import ScrollToTop from "../components/ScrollBtn";
 import ListeAvis from "../components/ListAvis";
+import { Helmet } from "react-helmet";
 
 
 function Home(){
     return(
         <div>
+            <Helmet>
+                <title> page d'accueil | Claude Henry - Hypnopraticien</title>
+                <meta name="description" content="Claude Henry - Hypnopraticien à Saint-Yvi, spécialiste en hypnose médicale et clinique et également évaluation et traitement de la douleur."/>
+                <meta name="keywords" content="Claude, Henry, hypnopraticien, hypnose, Saint-Yvi, Quimper, Ellian, Fouesnant, tabac, addictions, troubles, 
+                émotions, sommeil, phobies, douleur, alimentation, confiance en soi" />
+                <meta property="og:title" content="Page d'Accueil | Claude Henry - Hypnopraticien" />
+                <meta property="og:description" content="spécialiste en hypnose médicale et clinique, évaluation et traintement de la douleur" />
+                <meta property="og:image" content="https://www.monsite.com/assets/profil.webp" />
+                <meta property="og:url" content="https://www.claude-henry-hypnopraticien.fr" />
+            </Helmet>
             <Banner  title="Claude Henry" content="Hypnopraticien" backgroundImage={plage} alt="image de gallets"/>
             <Gallery/>
             <ListeAvis/>
