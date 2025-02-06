075acbe0e7f7d4abb7d006f794d51b162364b5e3
Wesley Goarant <Wesleydu29860@gmail.com>
2025-02-06 17:50:57 +0100

    création d'un back-end, avec node, express

---
+++ back-end/app.js
@@ -0,0 +1,26 @@
+// application express
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log('Requête reçue !');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: 'Votre requête a bien été reçue !' });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Réponse envoyée avec succès !');
+});
+
+//export de l'application pour l'utiliser dans les autres fichiers 
+module.exports = app;
