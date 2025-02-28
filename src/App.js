19d93a595fbaccd0660940aa5c3742efb06a83d2
Wesley Goarant <Wesleydu29860@gmail.com>
2025-02-28 11:43:53 +0100

    ajout d'une balise google, afin de recueillir les données visiteurs

--- src/App.js
+++ src/App.js
@@ -7,7 +7,18 @@ import APropos from "./pages/APropos";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Avis from "./pages/Avis";
+import { ReactGAImplementation } from "react-ga4";
+import { useLocation } from 'react-router-dom';
+
+ReactGAImplementation.initialize('G-67D73E3BXG');
+
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGAImplementation.send({ hitType: "pageview", page: location.pathname });
+  }, [location]);
+
   return (
     <Router>
       <Header/>
